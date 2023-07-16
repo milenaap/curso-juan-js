2d9cea0b220cfc3e2d36d9fff78b63c4de09394b
mile <darimile@gmail.com>
2023-07-16 20:47:42 +0200

    strings 3

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,13 @@
+const producto = 'Monitor 20 Pulgadas ';
+const precio = '30USD '
+
+// console.log(producto.concat(precio));
+// console.log(producto.concat('En descuento'));
+
+console.log(producto + "Con un precio de: " + precio);
+
+console.log("El producto " + producto + "tiene un precio de " + precio);
+console.log("El producto ", producto, "tiene un precio de ",precio);
+
+
+console.log(`El producto ${producto}tiene un precio de ${precio}`);
