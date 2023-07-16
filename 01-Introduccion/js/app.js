0a5165738ad6ca7765354d2cd5c19b63ef32cb33
mile <darimile@gmail.com>
2023-07-16 19:24:56 +0200

    first commit

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,20 @@
+// Pregunta al usuario su nombre
+// const nombre = prompt('Cual es tu nombre?');
+
+// Toma el nombre y lo muestra en pantalla
+// document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
+
+
+//Asigna el valor hacia la variable producto
+
+// const producto = 'Monitor 24 pulgadas';
+
+console.log('Hola');
+console.log('Mundo');
+
+function hola() {
+    console.log('ok');
+    console.log('Hola');
+    console.log('Mundo');
+}
+
