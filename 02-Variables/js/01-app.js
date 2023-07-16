8739fcfc4d3116dd5662476a20508399e1b2d6b2
mile <darimile@gmail.com>
2023-07-16 19:51:36 +0200

    add variables

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,39 @@
+// Inicializar una variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 Pulgadas';
+console.log(producto);
+
+// JavaScript es un lenguaje de tipo dinamico, no se espcifica tipo de dato
+producto = 20;
+console.log(producto);
+
+// Se pueden inicializar sin valor y asignarlo después
+var disponible;
+disponible = true;
+
+disponible = false;
+
+
+// Inicializar múltiples variables
+var categoria = 'Computadoras',
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+// Las variables no pueden iniciar con números
+var 99dias;
+var dias99;
+
+var 01_;
+var _01;
+
+var _usuario;
+var __usuario;
+
+// Como se escriben las variables
+var nombreProducto;
+var nombre_producto_categoria_precio;
+var Producto;
+
