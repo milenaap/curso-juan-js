8c99d1a1d092400fad8a7596f4c2007bae0e7b41
mile <darimile@gmail.com>
2023-07-17 20:45:40 +0200

    add string 05

---
+++ 03-Strings/js/05-app.js
@@ -0,0 +1,21 @@
+const producto = 'Monitor 20 Pulgadas ';
+
+console.log(producto);
+console.log(producto.replace('Pulgadas', '"'));
+console.log(producto.replace('Monitor', 'Monitor Curvo'));
+
+// .slide para cortar
+console.log(producto.slice(0, 10) )
+// console.log(producto.slice(8) );
+// console.log(producto.slice(2, 1) );
+
+
+
+//Alternativa a slide
+
+console.log(producto.substring(0, 10));
+console.log(producto.substring(2, 1));
+
+const usuario = "Milena";
+console.log(usuario.substring(0,10))
+console.log(usuario.charAt(0))
