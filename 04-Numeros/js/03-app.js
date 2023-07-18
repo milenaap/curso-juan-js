97e0bd56b3cd39ad1b9721a103097c329e4ecf1c
mile <darimile@gmail.com>
2023-07-18 21:17:30 +0200

    add string 04 numeros

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,61 @@
+let resultado;
+
+// PI
+
+resultado = Math.PI;
+
+
+//Redondear
+resultado = Math.round(2.5);
+resultado = Math.round(2.1);
+resultado = Math.round(2.7);
+
+
+
+// Redondear hacia arriba
+resultado = Math.ceil(2.1);
+resultado = Math.ceil(2.6);
+resultado = Math.ceil(2.8);
+
+
+// Redondear hacia abajo
+
+
+resultado = Math.floor(2.5);
+resultado = Math.floor(2.2);
+
+// Raíz Cuadrada
+
+resultado = Math.sqrt(144);
+
+
+// Absoluto
+
+resultado = Math.abs(-500);
+
+// Potencia
+
+resultado = Math.pow(2, 4);
+
+// Mínimo
+
+resultado = Math.min(3, 5, 1, 12, -3);
+
+
+// Máximo
+
+resultado = Math.max(3, 5, 1, 12, -3);
+
+//Aleatorio
+
+// resultado = Math.random() * 20;
+
+// Aleatorio dentro de un rango...
+
+resultado = Math.floor(Math.random() * 30);
+
+
+
+console.log(resultado);
+
+
---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,19 @@
+
+
+let puntaje = 5;
+
+
+puntaje += 3;
+puntaje -= 3;
+
+
+// aumentando
+puntaje++; // 5
+++puntaje; // 6
+
+
+// dismunuyendo
+puntaje--; // 5
+--puntaje; // 4
+
+console.log(puntaje);
