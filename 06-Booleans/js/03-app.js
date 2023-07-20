836defe4939b768164a39540648ee61eb098c76b
mile <darimile@gmail.com>
2023-07-20 19:29:35 +0200

    add booleans

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,12 @@
+const autenticado = true;
+
+if(autenticado === true) {
+    console.log('Si puedes ver netflix')
+}else {
+    console.log('No puedes verlo')
+}
+
+// Operador ternario
+
+
+console.log(autenticado ? 'Si esta' : 'No esta');
