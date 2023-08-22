1dc910a864fa029d548c93f4cdab6c51c9b046ca
mile <darimile@gmail.com>
2023-08-22 21:01:48 +0200

    add array methods

---
+++ 12-ArrayMethods/js/02-app.js
@@ -0,0 +1,29 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+meses.forEach((mes, i) => {
+    // console.log(i)
+    // console.log(mes)
+
+    if(mes === 'Abril') {
+        console.log(`Encontrado en el indice ${i}`)
+    }
+});
+
+// Encontrar el indice de Abril findIndex
+
+const indice = meses.findIndex(mes => mes === 'Abril');
+console.log(indice);
+
+// Encontrar un indice de un arreglo de objetos
+
+const indice2 = carrito.findIndex( producto => producto.precio === 100);
+console.log(indice2);
