15e88623183ea1b30dcc55545ed7380a8193d2f5
mile <darimile@gmail.com>
2024-07-08 22:12:27 +0200

    add 44 proyecto_

--- 44-PROYECTO-CRM-CRUD-REST/js/nuevocliente.js
+++ 44-PROYECTO-CRM-CRUD-REST/js/nuevocliente.js
@@ -1,3 +1,5 @@
+import { mostrarAlerta } from "./funciones.js";
+
 (function() {
     const formulario = document.querySelector('#formulario');
     formulario.addEventListener('submit', validarCliente);
@@ -22,7 +24,7 @@
         if(validar(cliente)) {
             //Mostrar mensaje
 
-            console.log('Todos los campos son obligatorios');
+            mostrarAlerta('Todos los campos son obligatorios');
             return;   
         }
 
